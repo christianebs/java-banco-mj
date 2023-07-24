f9b128eed23cc52348845041541baedc50e55eaf
christianebs <christianebs90@gmail.com>
2023-07-24 14:05:44 -0300

    criando listas e conjuntos da classe pessoa

---
+++ src/com/github/christianebs/banco/app/Principal2.java
@@ -0,0 +1,43 @@
+package com.github.christianebs.banco.app;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.github.christianebs.banco.modelo.Pessoa;
+
+public class Principal2 {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoa1 = new Pessoa("Maria da Silva", "123456789");
+        Pessoa pessoa2 = new Pessoa("João de Souza", "987654321");
+
+        //List<Pessoa> pessoas = new ArrayList<>();
+        Set<Pessoa> pessoas = new HashSet<>();
+        pessoas.add(pessoa1);
+        pessoas.add(pessoa2);
+
+        //System.out.println(pessoas.toString());
+
+        //Pessoa pessoa3 = pessoas.get(1);
+        //System.out.println(pessoa3);
+
+        //for (int i = 0; i < pessoas.size(); i++) {
+        //System.out.println(pessoas.get(i).getNome());
+        //}
+
+        //for (Pessoa pessoa : pessoas) {
+        //System.out.println(pessoa.getNome());
+        //}
+
+        Pessoa pessoa4 = new Pessoa("Maria da Silva", "123456789");
+
+        boolean existe = pessoas.contains(pessoa4);
+        System.out.println(existe);
+
+        // System.out.println(pessoa1.equals(pessoa4));
+    }
+
+}
